9efe7ca165a873933076f411a8634942f06da86c
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2020-04-26 00:18:11 -0300

    Feito configurações no arquivo. script.js

---
+++ 9. Introdução ao JavaScript/js/script.js
@@ -0,0 +1,14 @@
+//alert("Olá Mundo!");
+
+//var resposta = confirm("Desenha excluir?");
+//console.log(resposta);
+
+var a = 10;
+var b = 33;
+
+function somar(a, b){
+    return a + b;
+
+}
+
+console.log(somar(2,5));
