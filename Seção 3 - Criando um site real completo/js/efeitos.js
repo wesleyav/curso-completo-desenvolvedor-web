9e81ca4cfe26101df2c040ea9346236086de626e
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2020-05-03 19:47:00 -0300

    58. Responsivo Mobile - Menu - Parte 3

--- Seção 3 - Criando um site real completo/js/efeitos.js
+++ Seção 3 - Criando um site real completo/js/efeitos.js
@@ -1,52 +1,74 @@
 $(document).ready(function () {
 
     $("#logotipo").on("mouseover", function () {
+
         $("#banner h1").addClass("efeito");
 
     }).on("mouseout", function () {
+
         $("#banner h1").removeClass("efeito");
 
     });
+
     $("#input-search").on("focus", function () {
 
         $("li.search").addClass("ativo");
+
     }).on("blur", function () {
 
         $("li.search").removeClass("ativo");
+
     });
 
     $(".thumbnails").owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        navText: ["Anterior", "Próximo"],
-        responsive: {
-            0: {
-                items: 1
-            },
-            480: {
-                items: 2
-            },
-            768: {
-                items: 4
-            },
-            1200: {
-                items: 4
-            });
-            var owl = $(".thumbnails").data('owlCarousel');
-            $('#btn-news-prev').on("click", function(){
-                owl.prev();
-            });
 
-            $('#btn-news-next').on("click", function(){
-                owl.next();
-            });
-            $("#page-up").on("click", function(event))
-            
-            $("body").animate({
-               scrollTop:0 
-            }, 1000);
-            event.preventDefault();
-        )};
+        //autoPlay: 3000,
+        items: 4
+
     });
+
+    var owl = $(".thumbnails").data('owlCarousel');
+
+    $('#btn-news-prev').on("click", function () {
+
+        owl.prev();
+
+    });
+
+    $('#btn-news-next').on("click", function () {
+
+        owl.next();
+
+    });
+
+    $("#page-up").on("click", function (event) {
+
+        $("body").animate({
+            scrollTop: 0
+        }, 1000);
+
+        event.preventDefault();
+
+    });
+
+    $("#btn-bars").on("click", function () {
+
+        $("header").toggleClass("open-menu");
+
+    });
+
+    $("#menu-mobile-mask, .btn-close").on("click", function () {
+
+        $("header").removeClass("open-menu");
+
+
+    });
+
+    $("#btn-search").on("click", function () {
+
+        $("header").toggleClass("open-search");
+        $("#input-search-mobile").focus();
+
+    });
+
 });
